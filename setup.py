7b1cf01ce8dd1e7586ec45475ee8140eec61f333
Markus Unterwaditzer <markus@unterwaditzer.net>
2012-09-17 18:02:59 +0200

    New version

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 
 from setuptools import setup, find_packages
 from sys import version_info
+import pkg_resources
 
 dependencies = []
 
@@ -13,7 +14,7 @@ if version_info < (2,7):
 
 setup(
     name='webuntis',
-    version='0.1.0',
+    version='0.1.1',
     author='Markus Unterwaditzer',
     author_email='markus@unterwaditzer.net',
     packages=['webuntis', 'webuntis.tests'],
