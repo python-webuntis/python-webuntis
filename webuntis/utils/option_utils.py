c3a371d5fcd091dc2085ea76dfcdac02b33c2921
Markus Unterwaditzer <markus@unterwaditzer.net>
2012-10-24 14:22:47 +0200

    Fixed server option parser.

--- webuntis/utils/option_utils.py
+++ webuntis/utils/option_utils.py
@@ -22,7 +22,7 @@ def server(url):
         return url  # either it's None or we have a dictionary
 
     if not re.match(r'^http(s?)\:\/\/', url):  # if we just have the hostname
-        logging.debug('The URL given doesn\'t seem to be a valid URL, just'
+        logging.debug('The URL given doesn\'t seem to be a valid URL, just '
                       'gonna prepend "http://"')
 
         # append the http prefix and hope for the best
@@ -34,13 +34,13 @@ def server(url):
         # urlparse failed
         raise ValueError('Not a valid URL or hostname')
 
-    if not re.match(r'^[a-zA-Z0-9\.\:-]+$', urlobj.netloc):
+    if not re.match(r'^[a-zA-Z0-9\.\:-_]+$', urlobj.netloc):
         # That's not even a valid hostname
         raise ValueError('Not a valid hostname')
 
     if urlobj.path == '/':
-        logging.warning('You specified that the API endpoint should be /. That'
-                        'is uncommon. If you didn\'t mean to do so, remove'
+        logging.warning('You specified that the API endpoint should be /. That '
+                        'is uncommon. If you didn\'t mean to do so, remove '
                         'the slash at the end of your "server" parameter.')
 
     return urlobj.scheme + \
