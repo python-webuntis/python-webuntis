a8509032f5ff2dfae411dd743e9d8707f0371c23
Markus Unterwaditzer <markus@unterwaditzer.net>
2012-11-24 18:21:06 +0100

    Removed idiotic special behavior of our exceptions
    Make them just behave like builtin exceptions.

--- webuntis/errors.py
+++ webuntis/errors.py
@@ -10,13 +10,7 @@ from __future__ import unicode_literals
 
 class Error(Exception):
     '''Superclass for all `python-webuntis`-specific errors, never gets raised
-    directly. Positional arguments get joined with ``", "`` and get dumped into
-    ``self.msg``'''
-
-    def __init__(self, *args):
-        Exception.__init__(self, *args)
-        self.msg = ', '.join(args)
-
+    directly.'''
 
 class RemoteError(Error, IOError):
     '''There was some kind of error while interacting with the server.'''
