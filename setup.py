81265459f1b951623423070e385dcdb39b1f8b4f
Markus Unterwaditzer <markus@unterwaditzer.net>
2013-02-24 16:43:03 +0100

    Version 0.1.7

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ if version_info < (2,7):
 
 setup(
     name='webuntis',
-    version='0.1.6',
+    version='0.1.7',
     author='Markus Unterwaditzer',
     author_email='markus@unterwaditzer.net',
     packages=['webuntis', 'webuntis.utils', 'webuntis.tests'],
